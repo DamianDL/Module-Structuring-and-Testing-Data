d9ecdf53ede0092bd8aaba9933dbb6cd25f0822c
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-02-09 11:34:41 +0000

    Answer to 2-mandatory-errors/0.js  included as text

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,4 @@
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+
+//problem: the computer will try to run these lines and will throw an error because this is text and not code. We can solve this problem by commenting out these lines using the "//" notation at the beginning of the lines, so that the computer ignores them when running the program.
