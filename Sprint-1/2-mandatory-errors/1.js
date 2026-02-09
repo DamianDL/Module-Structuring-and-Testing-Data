aa7d600e400d642b45ef77c21276eb588c82184f
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-02-09 11:36:15 +0000

    Answer to 2-mandatory-errors/1.js  included as text

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,4 @@
 
 const age = 33;
 age = age + 1;
+//problem: we cannot reassign a value to a variable that has been declared with "const". We can solve this problem by changing the declaration of the variable from "const" to "let", which allows us to reassign values to the variable. So we would change the first line to "let age = 33;".
