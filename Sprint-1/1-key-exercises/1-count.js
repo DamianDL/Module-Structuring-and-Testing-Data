36a00cc62f5ce15da602ce1d21695ffced147863
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-02-09 10:59:06 +0000

    Key exercises 1-3 completed

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,7 @@
 let count = 0;
 
 count = count + 1;
-
+console.log(count); // Output: 1
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// Line 3 is an assignment statement that updates the value of the count variable. The expression on the right-hand side (count + 1) calculates the new value by taking the current value of count (which is 0) and adding 1 to it, resulting in 1. The = operator then assigns this new value back to the count variable, effectively updating it from 0 to 1. 
