6b83e2732c7c55606efbff611269246a7fef80bb
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-02-09 11:58:35 +0000

    Answer to 2-mandatory-errors/4.js code updated and explanation  included as text

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,12 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+// JavaScript variable names (identifiers) must not start with a digit.
+// They may only start with:a letter 
+// (A–Z or a–z)
+
+const T12HourClockTime = "20:53";
+const T24hourClockTime = "08:53";
+console.log (T12HourClockTime);
+console.log (T24hourClockTime);
+
+
