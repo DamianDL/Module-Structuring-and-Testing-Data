a893b0973eb5c561c56bb18a64df585979a7245e
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-02-25 15:37:44 +0000

    removing practice-tdd from branch

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,12 +1,5 @@
 function countChar(stringOfCharacters, findCharacter) {
- //
-  let count = 0;
-  for (let i = 0; i < stringOfCharacters.length; i++) {
-    if (stringOfCharacters[i] === findCharacter) {
-      count++;
-    }
-  }
-  return count;
+  return 5
 }
 
 module.exports = countChar;
--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,12 +1,5 @@
-function repeatStr(str, count) {
-  if (count < 0) {
-    throw new Error("Count must be a non-negative integer");
-  }
-  let result = "";
-  for (let i = 0; i < count; i++) {
-    result += str;
-  }
-  return result;
+function repeatStr() {
+  return "hellohellohello";
 }
 
 module.exports = repeatStr;
